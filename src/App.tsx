cfbbc4f72778e4ada7fb1ee19b756d093005985f
Nemanja Malesija <malesija13@gmail.com>
2023-03-05 23:57:30 +0100

    Fetch human species

--- src/App.tsx
+++ src/App.tsx
@@ -1,14 +1,24 @@
 import useGetCharacters from './helpers/useGetCharacters';
+import { gql, useQuery } from '@apollo/client';
+
+const GET_HUMANS = gql`
+  query {
+    characters(filter: { species: "human" }) {
+      results {
+        name
+      }
+    }
+  }
+`;
 
 function App() {
-  const { loading, error, data } = useGetCharacters();
+  const { loading, error, data } = useQuery(GET_HUMANS);
 
   if (loading) return <h1>Loading...</h1>;
 
   const {
     characters: { results },
   } = data;
-
   console.log(results);
 
   return <div className="App"></div>;
