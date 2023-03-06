4c1fb080a9e46cb03adfc9e7723f2bef90ce46e1
Nemanja Malesija <malesija13@gmail.com>
2023-03-06 03:59:53 +0100

    Link to specific character id

--- src/Home.tsx
+++ src/Home.tsx
@@ -1,4 +1,5 @@
 import useGetCharacters from './helpers/useGetCharacters';
+import { Link } from 'react-router-dom';
 
 type character = {
   id: number;
@@ -23,15 +24,17 @@ function Home() {
       {results.map((item: character) => {
         const { id, name, image, species } = item;
         return (
-          <article key={id} className="article__home">
-            <header className="article__home--header">
-              <img src={image} className="article__home--image" />
-            </header>
-            <div className="article__home--text-box">
-              <p className="article__home--char-name">{name}</p>
-              <p className="article__home--char-species">({species})</p>
-            </div>
-          </article>
+          <Link to={`/${id}`}>
+            <article key={id} className="article__home">
+              <header className="article__home--header">
+                <img src={image} className="article__home--image" />
+              </header>
+              <div className="article__home--text-box">
+                <p className="article__home--char-name">{name}</p>
+                <p className="article__home--char-species">({species})</p>
+              </div>
+            </article>
+          </Link>
         );
       })}
     </div>
--- src/SingleCharacter.tsx
+++ src/SingleCharacter.tsx
@@ -1,7 +1,12 @@
 import React from 'react';
+import { useParams } from 'react-router-dom';
 
 const SingleCharacter = () => {
-  return <div>SingleCharacter</div>;
+  const { id } = useParams();
+
+  console.log(id);
+
+  return <div>{id}</div>;
 };
 
 export default SingleCharacter;
