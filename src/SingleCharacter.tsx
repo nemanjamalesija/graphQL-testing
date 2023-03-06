dee8dc644d2b17cc41d5e529878e1e8c0cb1db70
Nemanja Malesija <malesija13@gmail.com>
2023-03-06 04:58:21 +0100

    Add styles to single fetched character

--- src/SingleCharacter.tsx
+++ src/SingleCharacter.tsx
@@ -2,6 +2,12 @@ import React from 'react';
 import { useParams } from 'react-router-dom';
 import { gql, useQuery } from '@apollo/client';
 
+type episode = {
+  id: string;
+  episode: string;
+  name: string;
+};
+
 const GET_SPECIFIC_CHARACTER = gql`
   query getSpecificCharacter($id: ID!) {
     character(id: $id) {
@@ -18,7 +24,6 @@ const GET_SPECIFIC_CHARACTER = gql`
 
 const SingleCharacter = () => {
   const { id } = useParams();
-  if (!id) return;
 
   const { error, loading, data } = useQuery(GET_SPECIFIC_CHARACTER, {
     variables: { id },
@@ -30,7 +35,27 @@ const SingleCharacter = () => {
     character: { name, image, episode },
   } = data ?? {};
 
-  return <div>{id}</div>;
+  return (
+    <article className="article__single">
+      <header className="article__single--image-header">
+        <img className="article__single--image" src={image} alt={name} />
+        <h3 className="article__single--name">{name}</h3>
+      </header>
+
+      <div className="article__single--featured-in-container">
+        {episode.map((ep: episode) => {
+          const { id, episode, name } = ep;
+
+          return (
+            <article key={id} className="article__episode">
+              <p className="article__episode--name">{name}</p>
+              <p className="article__episode--number">({episode})</p>
+            </article>
+          );
+        })}
+      </div>
+    </article>
+  );
 };
 
 export default SingleCharacter;
